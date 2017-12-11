80b585df44dc2ec1a7a577206a1ed02ac658d3a4
XavierLin <Xavier.lin@foxmail.com>
2017-12-11 18:51:29 +0800

    separate options

--- layers/modules/multibox_loss.py
+++ layers/modules/multibox_loss.py
@@ -95,7 +95,7 @@ class MultiBoxLoss(nn.Module):
         # Hard Negative Mining
         loss_c[pos] = 0  # filter out pos boxes for now
         loss_c = loss_c.view(num, -1)
-        print(loss_c)
+        # print(loss_c)
         _, loss_idx = loss_c.sort(1, descending=True)
         _, idx_rank = loss_idx.sort(1)
         num_pos = pos.long().sum(1, keepdim=True)
