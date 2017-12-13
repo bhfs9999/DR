d7c26fb29545f687d768efb64903570a27b7627c
XavierLin <Xavier.lin@foxmail.com>
2017-12-13 16:39:56 +0800

    done: data v0.1.0

--- train.py
+++ train.py
@@ -31,7 +31,8 @@ if __name__ == '__main__':
         existed_xmls = [fname.split('_')[0] for fname in os.listdir(args.xml_root)]
 
         all_idxes  = set(existed_imgs).intersection(set(existed_xmls))
-        all_xmlfnames = [idx+'_lable.xml' for idx in all_idxes]
+        all_xmlfnames = [idx+'_lable.xml' for idx in all_idxes][:1000]
+        sorted(all_xmlfnames)
         n_data       = len(all_xmlfnames)
         train_fnames = all_xmlfnames[:int(n_data*0.8)]
         val_fnames   = all_xmlfnames[int(n_data*0.8):]
